307a376a3b5777c397cce5b35d63c374f6536831
Gildeir <gildeirlopes@gmail.com>
2021-09-10 14:53:40 -0300

    importando express e body-parser

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // não remova esse endpoint, e para o avaliador funcionar
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
 app.get('/', (_request, response) => {
   response.send();
 });
